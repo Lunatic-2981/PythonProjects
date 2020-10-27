aa00a34eda1adbdd8cf44a8fa8c216425905a87a
Lunatic-2981 <68286737+Lunatic-2981@users.noreply.github.com>
2020-10-27 13:54:45 +0530

    Output of the 2nd project uploaded

---
+++ output of abc.py
@@ -0,0 +1,11 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:/Users/kabir/Desktop/abc.py ====================
+Input the Filename: abc.py
+The extension of the file is : ''
+>>> 
+==================== RESTART: C:/Users/kabir/Desktop/abc.py ====================
+Input the Filename: abc.py
+The extension of the file is : 'py'
+>>> 
