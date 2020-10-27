0295ec253d0a7aa0314d8f291e4773d0cebb327c
Lunatic-2981 <68286737+Lunatic-2981@users.noreply.github.com>
2020-10-27 13:46:48 +0530

    Uploaded the output of the code

---
+++ output.py
@@ -0,0 +1,7 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: C:\Users\kabir\Desktop\circle's area.py ===============
+Input the radius of the circle : 1.1
+The area of the circle with radius 1.1 is: 3.8013271108436504
+>>> 
